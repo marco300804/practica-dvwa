17210b635790050109d5e2da64b9929655056858
Marco <rtmarco2004@gmail.com>
2026-04-20 19:22:48 +0200

    Refactorizado switch a if y convertidos tabs a espacios

--- vulnerabilities/sqli/source/low.php
+++ vulnerabilities/sqli/source/low.php
@@ -1,59 +1,56 @@
 <?php
 
 if( isset( $_REQUEST[ 'Submit' ] ) ) {
-	// Get input
-	$id = $_REQUEST[ 'id' ];
+    // Get input
+    $id = $_REQUEST[ 'id' ];
 
-	switch ($_DVWA['SQLI_DB']) {
-		case MYSQL:
-            // Check database usando sentencias preparadas (seguro)
-            $stmt = $GLOBALS["___mysqli_ston"]->prepare("SELECT first_name, last_name FROM users WHERE user_id = ?");
-            $stmt->bind_param("s", $id); // "s" significa que el id es tratado como string
-            $stmt->execute();
-            $result = $stmt->get_result();
+    if ($_DVWA['SQLI_DB'] == MYSQL) {
+        // Check database usando sentencias preparadas (seguro)
+        $stmt = $GLOBALS["___mysqli_ston"]->prepare("SELECT first_name, last_name FROM users WHERE user_id = ?");
+        $stmt->bind_param("s", $id); 
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-            // Get results
-            while( $row = mysqli_fetch_assoc( $result ) ) {
+        // Get results
+        while( $row = mysqli_fetch_assoc( $result ) ) {
+            // Get values
+            $first = $row["first_name"];
+            $last  = $row["last_name"];
+
+            // Feedback for end user
+            $html .= "<pre>ID: " . htmlspecialchars($id) . "<br />First name: {$first}<br />Surname: {$last}</pre>";
+        }
+
+        $stmt->close();
+        mysqli_close($GLOBALS["___mysqli_ston"]);
+
+    } elseif ($_DVWA['SQLI_DB'] == SQLITE) {
+        global $sqlite_db_connection;
+
+        $query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
+        try {
+            $results = $sqlite_db_connection->query($query);
+        } catch (Exception $e) {
+            echo 'Caught exception: ' . $e->getMessage();
+            exit();
+        }
+
+        if ($results) {
+            while ($row = $results->fetchArray()) {
                 // Get values
                 $first = $row["first_name"];
                 $last  = $row["last_name"];
 
                 // Feedback for end user
-                $html .= "<pre>ID: " . htmlspecialchars($id) . "<br />First name: {$first}<br />Surname: {$last}</pre>";
+                $html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
             }
-
-            $stmt->close();
-            mysqli_close($GLOBALS["___mysqli_ston"]);
-            break;
-		case SQLITE:
-			global $sqlite_db_connection;
-
-			#$sqlite_db_connection = new SQLite3($_DVWA['SQLITE_DB']);
-			#$sqlite_db_connection->enableExceptions(true);
-
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			#print $query;
-			try {
-				$results = $sqlite_db_connection->query($query);
-			} catch (Exception $e) {
-				echo 'Caught exception: ' . $e->getMessage();
-				exit();
-			}
-
-			if ($results) {
-				while ($row = $results->fetchArray()) {
-					// Get values
-					$first = $row["first_name"];
-					$last  = $row["last_name"];
-
-					// Feedback for end user
-					$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
-				}
-			} else {
-				echo "Error in fetch ".$sqlite_db->lastErrorMsg();
-			}
-			break;
-	} 
+        } else {
+            echo "Error in fetch ".$sqlite_db->lastErrorMsg();
+        }
+    } else {
+        // Este es el caso "default" que pedía SonarCloud
+        $html .= "<pre>Error: Base de datos no soportada.</pre>";
+    }
 }
 
 ?>
